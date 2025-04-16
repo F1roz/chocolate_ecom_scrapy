9444268c3caa09e9072c782dc6705e7d6d17386b
Firoz fahim <52341506+F1roz@users.noreply.github.com>
2025-04-16 15:46:07 +0600

    connected db, installed proxy for bypassing

--- chocolatescraper/chocolatescraper/spiders/chocolatespider.py
+++ chocolatescraper/chocolatescraper/spiders/chocolatespider.py
@@ -1,18 +1,39 @@
 import scrapy
+from chocolatescraper.itemloaders import ChocolateProductLoader
+from chocolatescraper.items import ChocolateProduct  
 
 
-class ChocolatespiderSpider(scrapy.Spider):
-    name = "chocolatespider"
-    allowed_domains = ["chocolate.co.uk"]
-    start_urls = ["https://www.chocolate.co.uk/collections/all"]
+API_KEY = '6ee4bd09-86b6-474d-ac16-f083fbef98ec'
 
-    def parse(self, response):
-        products = response.css('product-item')
-        
-        for product in products:
-            yield {
-                'name' : product.css('a.product-item-meta__title::text').get(),
-                'price' : product.css('span.price').get().replace('<span class="price">\n              <span class="visually-hidden">Sale price</span>','').replace('</span>',''),
-                'url' : product.css('div.product-item-meta a').attrib['href']
-            }
-        
+def get_proxy_url(url):
+    payload = {'api_key': API_KEY, 'url': url}
+    proxy_url = 'https://proxy.scrapeops.io/v1/?' + urlencode(payload)
+    return proxy_url
+    
+class ChocolateSpider(scrapy.Spider):
+
+   # The name of the spider
+   name = 'chocolatespider'
+
+   # These are the urls that we will start scraping
+   #start_urls = ['https://www.chocolate.co.uk/collections/all']
+   
+   def start_requests(self):
+       start_url = 'https://www.chocolate.co.uk/collections/all'
+       yield scrapy.Request(get_proxy_url(start_url), callback=self.parse)
+   
+   def parse(self, response):
+       products = response.css('product-item')
+
+       for product in products:
+            chocolate = ChocolateProductLoader(item=ChocolateProduct(), selector=product)
+            chocolate.add_css('name', "a.product-item-meta__title::text")
+            chocolate.add_css('price', 'span.price', re='<span class="price">\n              <span class="visually-hidden">Sale price</span>(.*)</span>')
+            chocolate.add_css('url', 'div.product-item-meta a::attr(href)')
+            yield chocolate.load_item()
+
+       next_page = response.css('[rel="next"] ::attr(href)').get()
+
+       if next_page is not None:
+           next_page_url = 'https://www.chocolate.co.uk' + next_page
+           yield response.follow(get_proxy_url(next_page_url), callback=self.parse)
